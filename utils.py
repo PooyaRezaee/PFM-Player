5a4ade1a475fc842d86329708a5acdb1a191bd6f
pooya <pooya.rezaee.official@gmail.com>
2022-09-09 21:02:09 +0430

    Added class music play and controller

--- utils.py
+++ utils.py
@@ -1,5 +1,57 @@
 import json
+from pygame import mixer
 
+class Music():
+    def __init__(self,path):
+        self.mixer = mixer
+        self.music = self.mixer.music
+        self.path = path
+        self.IS_PLAY = False
+        self.IS_PLAYED = False
+
+        self.mixer.init()
+        self.music.load(self.path)
+    
+    def play(self):
+        self.music.play()
+        self.IS_PLAYED = True
+        self.IS_PLAY = True
+
+    def pause(self):
+        self.music.pause()
+        self.IS_PLAY = False
+
+    def unpause(self):
+        self.music.unpause()
+        self.IS_PLAY = True
+
+
+    def stop(self,fade=False):
+        self.IS_PLAY = False
+        if fade:
+            self.music.fadeout(800)
+        else:
+            self.music.stop()
+    
+
+    def set_volum(self,volum):
+        # 0 to 1
+        self.music.set_volume(volum)
+    
+    def set_pos(self,secound):
+        self.music.set_pos(secound)
+    
+    def get_volumn(self):
+        return self.music.get_volume()
+
+    def get_pos(self):
+        return self.music.get_pos()
+
+    def is_play(self):
+        return self.IS_PLAY
+
+    def is_played(self):
+        return self.IS_PLAYED
 
 def read_data_setting():
     with open('config.json', 'r') as file:
